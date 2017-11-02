2fb4d5b601cf6375fd14be714db6e726a4af9707
lindenb <plindenbaum@yahoo.fr>
2017-11-02 15:55:33 +0100

    xml output

--- src/main/java/controller/Search.java
+++ src/main/java/controller/Search.java
@@ -1,13 +1,21 @@
 package controller;
 
 import java.io.IOException;
+import java.io.OutputStream;
+import java.util.List;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.xml.stream.XMLOutputFactory;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.XMLStreamWriter;
+
 import model.Download;
 import model.Output;
 import model.Upload;
+import object.VariantGeneScore;
 import util.DBManager;
 
 /**
@@ -51,7 +59,19 @@ public class Search extends HttpServlet {
                 setRequest(request, query);
             }
 
-            request.getRequestDispatcher("index.jsp").forward(request, response);
+            final String content_type = request.getParameter("content-type");
+            if("text/xml".equals(content_type) && 
+            		Upload.uploadErrMsg!=null &&
+            		Output.isRegionValid &&
+            		!Download.isDownloadOnly
+            		)
+            	{
+            	writeVariantsAsXml(request, response, Output.variantGeneScoreList);
+            	}
+            else
+            	{
+            	request.getRequestDispatcher("index.jsp").forward(request, response);
+            	}
         } catch (Exception ex) {
 //            request.setAttribute("errorMsg4Debug", ex.toString());
             request.getRequestDispatcher("error.jsp").forward(request, response);
@@ -84,4 +104,39 @@ public class Search extends HttpServlet {
     public String getServletInfo() {
         return "trap search query";
     }
+    
+    /** output a variant list as XML */
+    private void writeVariantsAsXml(
+    		final HttpServletRequest request,
+    		final HttpServletResponse response,
+    		final List<VariantGeneScore> variants
+    		) throws ServletException,IOException {
+    	String encoding = request.getCharacterEncoding();
+    	if(encoding==null || encoding.trim().isEmpty()) encoding = "UTF-8";
+    	response.setContentType("text/xml");
+    	final OutputStream out = response.getOutputStream();
+    	try {
+    		final XMLOutputFactory xmlOutputFactory = XMLOutputFactory.newFactory();
+    		final XMLStreamWriter w= xmlOutputFactory.createXMLStreamWriter(out,encoding);
+    		w.writeStartDocument(encoding, "1.0");
+    		w.writeStartElement("trap");
+    		w.writeComment("TraP is protected by copyright. 2017 The Trustees of Columbia University in the City of New York.");
+    		if(variants!=null)
+	    		{
+	    		for(final VariantGeneScore variant: variants)
+	    			{
+	    			variant.writeAsXml(w);
+	    			}
+	    		}
+    		w.writeEndElement();//close element 'trap'
+    		w.writeEndDocument();
+    		w.flush();
+    		w.close();
+    		}
+    	catch(final XMLStreamException err)
+    		{
+    		throw new IOException(err);
+    		}
+    	try {out.flush();} catch(IOException err) {} 
+    	}
 }
